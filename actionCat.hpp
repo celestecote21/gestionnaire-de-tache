d3dbae5981f427c4727331fd041fc403404c50f1
celeste <celeste@cote21.fr>
2019-06-06 23:27:23 +0200

    bug des string qui declenchais une boucle est resolue

--- actionCat.hpp
+++ actionCat.hpp
@@ -11,4 +11,5 @@ std::string getName(std::string strTache);
 Date toDate(std::string strDate);
 enum Statue {DAY, MONTH, HOUR, MINUTE, END};
 int toNbr(int dizaine, int unite);
+int strToInt(std::string str);
 #endif
